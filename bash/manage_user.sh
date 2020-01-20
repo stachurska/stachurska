621cd3338d729517c7aec7f2a77e8e0531d236ec
pzagrodnik <pawel23345@gmail.com>
2020-01-20 11:29:33 +0100

    finish script

--- bash/manage_user.sh
+++ bash/manage_user.sh
@@ -3,6 +3,19 @@
 #Zmienna globalna
 #ładowanie użytkowników
 user_list=(`cat users.txt`)
+status="FAIL"
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[1;32m'
+
+
+function checkStatusCode() {
+    if  [ $? == 0 ]; then
+	status="PASS"
+    else
+	status="FAIL"
+    fi
+}
 
 function showUsers() {
     echo "showUsers ..."
@@ -14,17 +27,19 @@ function showUsers() {
 }
 
 function addUsers() {
-    echo "addUsers ..."
+    echo  "addUsers ..."
     echo -n "Are you sure? [y/n]"
     read sure
     if [ "${sure}" == "y" ]; then
 	for user in "${user_list[@]}"
 	do
-	    echo "Add user: ${user}"
-	    sudo useradd ${user} -s /sbin/nologin -g "users"
+	     sudo useradd ${user} -m -s /sbin/nologin -g "users" 2> /dev/null
+	     checkStatusCode
+	    echo "Add user: ${user} [${status}]"    
 	done
     fi
 }
+
 
 
 function delUsers() {
@@ -34,19 +49,23 @@ function delUsers() {
     if [ "${sure}" == "y" ]; then
 	for user in "${user_list[@]}"
 	do
-	    echo "Remove user ${user} [OK]"
-	    sudo userdel ${user}
+	    sudo userdel -r ${user} 2> /dev/null
+	    checkStatusCode
+	    echo "Remove user ${user} [${status}]"   
 	done
     fi
 }
+
+
 
 
 function acceptRemoteLogin() {
     echo "acceptRemotLogin ..."
     for user in "${user_list[@]}"
     do
-    echo "Accept remote Login for ${user} [OK]"
-    sudo usermod -s /bin/bash ${user}
+	sudo usermod -s /bin/bash ${user} 2> /dev/null
+        checkStatusCode
+	echo "Accept remote Login for ${user} [${status}]"
     done
 }
 
@@ -54,10 +73,10 @@ function deniRemoteLogin() {
     echo "deniRemotUsers ..."
     for user in "${user_list[@]}"
     do
-	echo "Denied remote Login for ${user} [OK]"
-	sudo usermod -s /sbin/nologin ${user}
+	sudo usermod -s /sbin/nologin ${user} 2> /dev/null
+	checkStatusCode
+	echo "Denied remote Login for ${user} [${status}]"
     done
-
 }
 
 function quit() {
